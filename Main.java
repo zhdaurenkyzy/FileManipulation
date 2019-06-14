f5a8102f6ace145bf709866f35e3a0a13202e6e7
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-06-15 00:01:31 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите действие: \n 1 - копировать; \n 2 - переместить; \n 3 - удалить. ");
+        int menu = scanner.nextInt();
+        System.out.println("Укажите файл");
+        String fileName = scanner.next();
+        switch (menu) {
+            case 1:
+                System.out.println("Укажите путь");
+                String way = scanner.next();
+                FileManipulation.copy(fileName, way);
+                break;
+            case 2:
+                System.out.println("Укажите путь");
+                String wayForMoving = scanner.next();
+                FileManipulation.move(fileName, wayForMoving);
+                break;
+            case 3:
+                FileManipulation.delete(fileName);
+        }
+    }
+}
